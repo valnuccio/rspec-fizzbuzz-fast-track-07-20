db5060370d2440736902a7f49220bdb133212c96
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-20 14:20:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,2 @@
-def fizzbuzz
-  end# Don't forget! This file needs to be 'required' in its spec file
+# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
